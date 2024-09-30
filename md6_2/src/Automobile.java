ef4dcf0ef3d20c2f79687fe48ad50127967e46a8
KycT <magaaaaa666@mail.ru>
2024-09-30 11:14:22 +0300

    Made 2 task

---
+++ md6_2/src/Automobile.java
@@ -0,0 +1,11 @@
+class Automobile extends TexlaCar{
+    public Automobile() {
+        wheelsNumber = 4;
+    }
+
+    protected String direction;
+
+    public void turnTo(String worldSide) {
+        direction = worldSide;
+    }
+}
